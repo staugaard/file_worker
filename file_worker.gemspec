372ce77be7790d13d51b9841c0b4fb6fcf972d66
Morten Primdahl <morten@zendesk.com>
2012-01-25 15:43:54 -0800

    Worker tests

--- file_worker.gemspec
+++ file_worker.gemspec
@@ -20,6 +20,7 @@ Gem::Specification.new do |s|
   s.add_development_dependency "mocha"
   s.add_development_dependency "fakefs"
 
+  s.add_runtime_dependency "jruby-openssl"
   s.add_runtime_dependency "girl_friday"
   s.add_runtime_dependency "fog"
 end
--- test/worker_test.rb
+++ test/worker_test.rb
@@ -1,28 +1,32 @@
 require File.expand_path("test_helper", File.dirname(__FILE__))
 
 describe "a worker" do
+  before do
+    @file_name = "/path/to/file.name"
+  end
+
   it "should fail when not provided auth options" do
-    assert_raises(IndexError) { FileWorker::Worker.new("/path/to/file.name", {}) }
+    assert_raises(IndexError) { FileWorker::Worker.new(@file_name, {}) }
   end
 
   describe "instantiated correctly" do
     before do
-      @options   = {
+      @options = {
         :aws_access_key_id     => "some key",
         :aws_secret_access_key => "other key"
       }
 
-      @file_name = "/path/to/file.name"
-      @worker    = FileWorker::Worker.new(@file_name, @options.merge(:aws_s3_bucket => "files"))
+      @worker = FileWorker::Worker.new(@file_name, @options.merge(:aws_s3_bucket => "files"))
     end
 
     describe "process" do
       it "should attempt to transfer to the location" do
+        file   = stub()
+        File.expects(:open).with(@file_name).returns(file)
+
         files  = stub()
         bucket = stub(:files => files)
-        files.expects(:create).with({
-
-        })
+        files.expects(:create).with(:key => File.basename(@file_name), :body => file, :public => false)
 
         @worker.expects(:bucket).returns(bucket)
         @worker.process
