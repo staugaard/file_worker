0533ec6a54162c5cf05499da558c93b190a82592
Mick Staugaard <mick@staugaard.com>
2012-01-26 13:19:54 -0800

    the executable now works!

--- lib/file_worker.rb
+++ lib/file_worker.rb
@@ -4,4 +4,5 @@ module FileWorker
   autoload :DirectoryScanner, 'file_worker/directory_scanner'
   autoload :DefaultWorker,    'file_worker/default_worker'
   autoload :S3UploadWorker,   'file_worker/s3_upload_worker'
+  autoload :Cli,              'file_worker/cli'
 end
